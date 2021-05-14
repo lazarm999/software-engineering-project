d8644d86dfa6f5719e6f14c1ebff2a4fdbfb1cc8
Lazar Minic <lazarminic@elfak.rs>
2021-05-14 11:14:49 +0200

    Lazar merged to dev

--- Aplikacija/Android/app/src/main/java/com/parovi/zadruga/Constants.java
+++ Aplikacija/Android/app/src/main/java/com/parovi/zadruga/Constants.java
@@ -1,5 +1,7 @@
 package com.parovi.zadruga;
 
+import com.parovi.zadruga.models.User;
+
 public class Constants {
     public static final String FCM_BASE_URL = "https://fcm.googleapis.com";
     public static final String FB_SERVER_KEY = "AAAAVP04i60:APA91bEA7VRyCExMR_9BImCJqZ0b1FGG6R-V0keGGpxqoc_uLdnN9t_uwyMvRZ7rmHgptekr1Z0fiLjag7BzWGxNG9E3xEBgkndCEkhYEFEsbNS7ArPMUZtzbUdCtTufBkUXM5LnyeKw";
