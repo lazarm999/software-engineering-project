82e264ebcdfcab12e352c51d7c2e819b0b6a792f
Vuk Bibić <vuk.bibic@elfak.rs>
2021-06-30 21:33:37 +0200

    brisanje fcm tokena kad je banovan

--- Aplikacija/Python/ZadrugaAPI/api/exception_handler.py
+++ Aplikacija/Python/ZadrugaAPI/api/exception_handler.py
@@ -1,3 +1,4 @@
+from api.models import UserFCM
 from api.permissions import BannedException
 from api.responses import r408
 from rest_framework.views import exception_handler
@@ -6,4 +7,8 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     
     if isinstance(exc, BannedException):
+        try:
+            UserFCM.objects.filter(user__userId=context['request']._auth).delete()
+        except:
+            pass
         return r408()
